5c06457653d815b60db9bf88bc8622bf047f4ec9
LucasFurer <lucas.furer@gmail.com>
2026-08-17 17:27:34 +0200

    added blender toml file

--- run.py
+++ run.py
@@ -1,11 +1,10 @@
-import os
-import sys
+# import os
+# import sys
 
-sys.path.append(os.path.dirname(__file__))
+# sys.path.append(os.path.dirname(__file__))
 
-import pip
-
-pip.main(["install", "-r", f"{os.path.dirname(__file__)}/requirements.txt"])
+# import pip
+# pip.main(["install", "-r", f"{os.path.dirname(__file__)}/requirements.txt"])
 
 import sections
 
